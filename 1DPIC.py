8dc99e70f083cf00da11e5a14e07e93b2d3d091f
Dominik <stanczakdominik@gmail.com>
2015-04-21 08:58:38 +0200

    Temperature with plotting. Physical constants. Plot size.

--- 1DPIC.py
+++ 1DPIC.py
@@ -4,22 +4,39 @@ import matplotlib.pyplot as plt
 from numpy import sin, cos, exp, pi
 from numpy.fft import fft, ifft, fftfreq
 from datetime import datetime
-from ScienceConstants import *
+#
+from pylab import rcParams
+rcParams['figure.figsize'] = 20, 10
+
+#PHYSICAL CONSTANTS
+
+C=299792458. #m/s
+EPSILON0=8.854187817e-12 #farads/m
+ELECTRONCHARGE=1.602176565e-19 #kg
+ELECTRONMASS=9.10938215e-31 #kg
+PROTONMASS=1.672621777e-27 #kg
+BOLTZMANNKB=1.380648e-23 #J/K
+##BOLTZMANNKBEV=8.6173324e-5 #eV/K
+
+#SIMULATION PARAMETERS
 Dim=1
 N=100
 NG=10
-Size=np.array([30.])
-DT=0.1
+Size=np.array([1.])
+DT=0.0001
 np.random.seed(1)
-C=3e8
-eps_0=1
-RUNITERS=300
-ELEMCHARGE=10.
-SnapshotEveryXIterations=1
+RUNITERS=500
+TIMEITERS=np.arange(RUNITERS+1)*DT
 
-ESTMAXCHARGE=N*ELEMCHARGE/Size[0]/2
-ESTMAXFIELD=N*ELEMCHARGE/Size[0]/NG/Size[0]
-INITSHOWDENS=False
+PARTICLESPERSUPER=1
+SUPERPARTICLECHARGE=PARTICLESPERSUPER*ELECTRONCHARGE #for superparticles
+SUPERPROTONMASS=PARTICLESPERSUPER*PROTONMASS
+SUPERELECTRONMASS=PARTICLESPERSUPER*ELECTRONMASS
+SnapshotEveryXIterations=100
+
+ESTMAXCHARGE=N*SUPERPARTICLECHARGE/Size[0]/2/ELECTRONCHARGE/4
+ESTMAXFIELD=N*SUPERPARTICLECHARGE/Size[0]/NG/Size[0]
+INITSHOWDENS=True
 
 time=datetime.now().time()
 from time import gmtime, strftime
@@ -48,13 +65,13 @@ class grid(object):
     def densityplot(self, filename, view=False):
         plt.title("Grid")
         plt.subplot(2,1,1)
-        plt.plot(self.X, self.density, label="Density")
-        plt.ylabel("Charge density")
+        plt.plot(self.X, self.density/ELECTRONCHARGE, label="Density")
+        plt.ylabel("Charge density [elementary charge]")
         plt.ylim(-ESTMAXCHARGE, ESTMAXCHARGE)
         plt.subplot(2,1,2)
         plt.plot(self.X,self.efield, label="eField")
         plt.ylabel("Field")
-        plt.ylim(-ESTMAXFIELD, ESTMAXFIELD)
+##        plt.ylim(-ESTMAXFIELD, ESTMAXFIELD)
         plt.xlabel("Position on the grid")
         plt.savefig(filename)
         if view:
@@ -98,7 +115,7 @@ class grid(object):
         #FOURIER TRANSFORM
         self.freq=self.L*fftfreq(NG, self.dX)
         self.freq[0]=0.01
-        self.pot = np.real(ifft(fft(self.density)[0:NG]/self.freq[0:NG]**2/4./pi**2/eps_0))
+        self.pot = np.real(ifft(fft(self.density)[0:NG]/self.freq[0:NG]**2/4./pi**2/EPSILON0))
         self.efield = -np.gradient(self.pot)
         self.efield+=externalfield(self.X)
 class species(object):
@@ -110,8 +127,10 @@ class species(object):
         self.charge=charge
         self.position=position
         self.velocity=velocity
+
         self.trajectories=np.copy(self.position)
         self.velocities=np.copy(self.velocity)
+        self.temperatures=np.array(self.temperature())
     def move(self, dt):
         self.position += self.velocity*dt
         #wymuszanie warunków brzegowych
@@ -140,6 +159,8 @@ class species(object):
         acceleration[:,0]=self.charge*EField[:,0]/self.mass/(RelativisticCorrectionGamma(self.velocity))
 
         self.velocity+=acceleration*dt
+    def temperature(self):
+        return 0.5*self.mass*np.sqrt(np.sum(self.velocity**2)/self.number)
     def info(self):
         print "N=",self.number, self.name
         print "m=",self.mass, "q=", self.charge
@@ -150,26 +171,34 @@ class species(object):
         self.move(DT)
         self.trajectories=np.hstack((self.trajectories,self.position))
         self.velocities=np.hstack((self.velocities,self.velocity))
+        self.temperatures=np.hstack((self.temperatures,self.temperature()))
 def PlotAllTrajectories(ListOfSpecies):
     plt.title("Run history")
     for index, i in enumerate(ListOfSpecies):
-        plt.subplot(len(ListOfSpecies)+1,1,index+1)
-        plt.plot(i.velocities.T, color=i.color, alpha = 0.5)
-        plt.ylabel('X velocity for ' + i.name)
+        plt.subplot(len(ListOfSpecies)+2,1,index+1)
+        plt.plot(TIMEITERS, i.velocities.T, color=i.color, alpha = 0.8)
+        plt.ylabel('X velocity for ' + i.name + '[m/s]')
 
-    plt.subplot(len(ListOfSpecies)+1,1,len(ListOfSpecies)+1)
+    plt.subplot(len(ListOfSpecies)+2,1,len(ListOfSpecies)+1)
     for i in ListOfSpecies:
-        plt.plot(i.velocities.T, color=i.color, alpha = 0.5)
-    plt.ylabel('X velocity')
-    plt.xlabel("Iterations")
+        plt.plot(TIMEITERS, i.velocities.T, color=i.color, alpha = 0.5)
+    plt.ylabel('X velocity [m/s]')
+
+    plt.subplot(len(ListOfSpecies)+2,1,len(ListOfSpecies)+2)
+    for i in ListOfSpecies:
+        plt.plot(TIMEITERS, i.temperatures/BOLTZMANNKB, color=i.color)
+    plt.ylabel("Temperature [K]")
+    plt.xlabel("Time [s]")
     plt.savefig(RUNTIME + "Runhistory.png")
     plt.show()
 
 Grid=grid()
-electrons=species(1., -ELEMCHARGE, np.random.random((N,Dim))*Size, np.random.random((N,Dim))*Size-Size/2, N, "electrons", "y")
-protons=species(1., ELEMCHARGE, np.random.random((N,Dim))*Size, np.random.random((N,Dim))*Size-Size/2, N, "protons", "b")
-##electrons=species(1., -ELEMCHARGE, np.random.random((N,Dim))*Size, np.zeros((N,Dim)), N, "electrons", "y")
-##protons=species(1., ELEMCHARGE, np.random.random((N,Dim))*Size, np.zeros((N,Dim)), N, "protons", "b")
+#hot start
+electrons=species(SUPERELECTRONMASS, -SUPERPARTICLECHARGE, np.random.random((N,Dim))*Size, np.random.random((N,Dim))*Size-Size/2, N, "electrons", "y")
+protons=species(SUPERPROTONMASS, SUPERPARTICLECHARGE, np.random.random((N,Dim))*Size, np.random.random((N,Dim))*Size-Size/2, N, "protons", "b")
+#cold start
+##electrons=species(1., -SUPERPARTICLECHARGE, np.random.random((N,Dim))*Size, np.zeros((N,Dim)), N, "electrons", "y")
+##protons=species(1., SUPERPARTICLECHARGE, np.random.random((N,Dim))*Size, np.zeros((N,Dim)), N, "protons", "b")
 
 Species=[protons,electrons]
 Grid.update(Species)
@@ -178,6 +207,7 @@ for i in Species:
 for iterat in range(RUNITERS):
     for i in Species:
         i.step()
+##        print i.name, i.temperature()
     if iterat%SnapshotEveryXIterations==0:
         Grid.densityplot(RUNTIME + "DensityIter%d.png" %(iterat))
     Grid.update(Species)
